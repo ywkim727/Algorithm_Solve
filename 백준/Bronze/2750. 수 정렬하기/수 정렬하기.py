a02f93a73dd5db35ee1ba3931d6eef6ea766043d
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2023-04-09 21:57:57 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,3 +1,5 @@
+#버블정렬 알고리즘
+
 case = int(input())
 
 num_list = []
@@ -6,7 +8,12 @@ for _ in range(case) :
     num = int(input())
     num_list.append(num)
 
-num_list.sort()
+n = len(num_list)
+
+for i in range(n-1) :
+    for j in range(n-1, i, -1) :
+        if num_list[j-1] > num_list[j] :
+            num_list[j-1], num_list[j] = num_list[j], num_list[j-1]
 
 for i in num_list :
     print(i)
