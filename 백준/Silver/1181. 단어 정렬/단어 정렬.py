259a406879ca1efbcc116ae11561f422da087315
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2023-04-12 00:57:20 +0900

    [Silver V] Title: 단어 정렬, Time: 964 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,18 @@
+case = int(input())
+word = []
+
+for _ in range(case) :
+    word.append(input())
+
+set_word = list(set(word))
+
+sort_word = []
+
+for i in set_word :
+    sort_word.append((len(i), i))
+
+result = sorted(sort_word)
+
+for len_word, word in result :
+    print(word) 
+
