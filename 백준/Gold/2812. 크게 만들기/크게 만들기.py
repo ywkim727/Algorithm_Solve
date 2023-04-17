92723ff7c3630e950020bcd2cf6faace58b336d2
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2023-04-17 23:40:11 +0900

    Update 크게 만들기.py

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -7,16 +7,15 @@ num = sys.stdin.readline().rstrip()
 stack = []
 
 for i in num :
-    while stack and K > 0 and stack[-1] < i :
-        stack.pop()
+    while stack and K > 0 and stack[-1] < i :   #num에 있는 숫자를 stack에 넣고 다음 숫자와 비교, 다음 숫자가 stack에 있는 수보다 크면
+        stack.pop()                             #stack[-1]에 더 큰 수가 나올때까지 pop을 하면서 다음 숫자를 앞에 배치 
         K -= 1
     stack.append(i) 
 
-if K > 0 :
-    print(''.join(stack[:-K]))
+if K > 0 :                                      #만약 숫자를 덜 지웠다면
+    print(''.join(stack[:N-K]))                 #뒤의 숫자를 남은 k개 만큼 지우고 출력
 else :
     print(''.join(map(str,stack)))
-    
     
         
         
