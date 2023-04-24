e5f1682db00f50b56cf8319b37489ba7eb0ced2d
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2023-04-24 23:48:04 +0900

    [Gold V] Title: 최소비용 구하기, Time: 308 ms, Memory: 126244 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,29 @@
+import sys
+from heapq import heappop, heappush
+#다익스트라 알고리즘 풀이
+def dijkstra(start, end) :
+    distance = [int(1e9)] * (N+1)
+    heap = []
+    heappush(heap, (0, start))
+    distance[start] = 0
+
+    while heap :
+        weight, index = heappop(heap)
+        if distance[index] < weight :
+            continue
+        for e, c in bus[index] :
+            if distance[e] > weight + c :
+                distance[e] = weight + c
+                heappush(heap,(weight+c, e))
+    return distance[end]
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+
+bus = [[] for _ in range(N+1)]
+for _ in range(M) :
+    s, e, cost = map(int, sys.stdin.readline().split())
+    bus[s].append((e, cost))
+
+start, end = map(int, sys.stdin.readline().split())
+print(dijkstra(start, end))
