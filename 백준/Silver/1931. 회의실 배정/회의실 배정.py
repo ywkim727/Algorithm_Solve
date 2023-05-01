73853ef22146219b3463f9e6f50a94042f4ac953
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2023-05-01 13:13:09 +0900

    [Silver I] Title: 회의실 배정, Time: 6652 ms, Memory: 45892 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,23 @@
+# 1. 회의 시작 시간으로 오름차순 
+# 2. 1 의 결과에서 끝나는 시간으로 다시 오름차순
+
+N = int(input())
+
+time = []
+
+for _ in range(N) :
+    start, end = map(int, input().split())
+    time.append((start, end))
+
+time.sort(key=lambda x : x[0])
+time.sort(key=lambda x : x[1])
+
+count = 0
+last = 0
+
+for i, j in time :
+    if i >= last :
+        last = j
+        count += 1
+
+print(count)
