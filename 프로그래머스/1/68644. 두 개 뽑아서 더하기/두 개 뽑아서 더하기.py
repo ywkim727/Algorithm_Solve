bf65650b5f2157ec1ad169f7039440337d7a5caf
YeongWoo Kim <114053340+ywkim727@users.noreply.github.com>
2024-04-07 14:22:38 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,16 +1,12 @@
-import itertools 
-
 def solution(numbers):
-    
     answer = []
     
-    com = list(itertools.combinations(numbers, 2))
-    
-    for i in com :
-        hap = int(i[0]) + int(i[1])
-        if hap not in answer :
-            answer.append(hap)
-            
+    for i in range(len(numbers)) :
+        for j in range(i+1, len(numbers)) :
+            hap = int(numbers[i]) + int(numbers[j])
+            if hap not in answer :
+                answer.append(hap)
+                
     answer.sort()
     
     return answer
